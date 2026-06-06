a143d9f67ecb3c4ccd47fc449f28787428df3b1d
adeedkhann <adeedkhan0786@gmail.com>
2026-06-06 21:12:56 +0530

    is i am too naive ?

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,12 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import {UserButton} from "@/modules/authentication/components/userButton"
-import { authClient } from "@/lib/auth-client";
 import { currentUser } from "@/modules/authentication/actions";
-import ChatSidebar from "@/modules/chat/components/chat-sidebar";
 import ChatMessageView from "@/modules/chat/components/chat-view/chat-message-view";
 
-
-
-const user = await currentUser()
 export default async function Home() {
+  const user = await currentUser();
 
   return (
     <div>
-      <ChatMessageView user={user}/>
+      <ChatMessageView user={user} />
     </div>
   );
 }
